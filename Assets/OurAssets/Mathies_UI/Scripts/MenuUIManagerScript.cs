e6753eea0707e781a8c0a85769208b620df6ef2c
Mathies Wiencke <mathieswiencke@hotmail.com>
2018-09-24 11:31:54 +0200

    Starting work on MainMenu

---
+++ Assets/OurAssets/Mathies_UI/Scripts/MenuUIManagerScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuUIManagerScript : MonoBehaviour {
+
+    private GameObject[] menuPanels;
+    private int currentActivePanel;
+
+	// Use this for initialization
+	void Start () {
+        menuPanels = GameObject.FindGameObjectsWithTag("MenuPanel");
+
+        foreach (GameObject panel in menuPanels)
+        {
+            panel.SetActive(false);
+        }
+        //Make the Main Menu-panel the last panel in the list so index is length-1.
+        currentActivePanel = menuPanels.Length-1;
+        menuPanels[2].SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void SetPanelActive(int panelIndex)
+    {
+        menuPanels[currentActivePanel].SetActive(false);
+        menuPanels[panelIndex].SetActive(true);
+        currentActivePanel = panelIndex;
+    }
+
+    public void StartGame(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
