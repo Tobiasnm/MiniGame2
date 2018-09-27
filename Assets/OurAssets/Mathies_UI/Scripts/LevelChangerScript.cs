259f3a1984e0c50a9bef582173c2bbf878ce0d7a
Mathies Wiencke <mathieswiencke@hotmail.com>
2018-09-27 14:37:56 +0200

    Made a small fading animation which can be called on any scene
    Made part of prefab so assigning should hopefully not be a problem.

---
+++ Assets/OurAssets/Mathies_UI/Scripts/LevelChangerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChangerScript : MonoBehaviour {
+
+    private Animator animator;
+
+    private string scenename;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void FadeToLevel (string levelname)
+    {
+        if (Time.timeScale == 0) Time.timeScale = 1f;
+        scenename = levelname;
+        animator.SetTrigger("menu_fade_out");
+    }
+
+    public void FadeComplete()
+    {
+        SceneManager.LoadScene(scenename);
+    }
+
+    public void FadeMenuIn()
+    {
+        animator.SetTrigger("menu_fade_in");
+    }
+}
