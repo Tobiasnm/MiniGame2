19632beb07ff644d48dac0bb57111569c6d9be9e
GeorgSatyros <georg.h.k@hotmail.com>
2018-10-01 13:39:28 +0200

    obstacle sounds added

--- Assets/OurAssets/Gameplay/Core/Local_Prefabs/Obstacles/ObstacleHandler.cs
+++ Assets/OurAssets/Gameplay/Core/Local_Prefabs/Obstacles/ObstacleHandler.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class ObstacleHandler : MonoBehaviour
 {
-    [Range(0,1)]
-    public float triggerEventAtPercentage = 0.0f;
-
+    //[Range(0,1)]
+    //public float triggerEventAtPercentage = 0.0f;
+    [Header("Events need to be in order from first to last")]
+    public MusicEvent[] events;
+    private Queue<MusicEvent> eventQueue = new Queue<MusicEvent>();
+    private MusicEvent nextEvent;
+    MusicEvent latestEvent;
     private Queue<GameObject> obstacles = new Queue<GameObject>();
 
     [SerializeField]
@@ -18,24 +22,45 @@ public class ObstacleHandler : MonoBehaviour
     [SerializeField]
     string tapSound;
 
+    private GameObject ambienceSoundBank;
+
     // Use this for initialization
     void Start()
     {
+        ambienceSoundBank = GameObject.FindGameObjectWithTag("AmbienceSoundBank");
+
         MeshRenderer[] renderedChildren = GetComponentsInChildren<MeshRenderer>();
         foreach (MeshRenderer child in renderedChildren)
         {
             obstacles.Enqueue(child.gameObject);
             Debug.Log(child.name);
         }
-        
+        if (events.Length <= 0)
+            eventQueue = new Queue<MusicEvent>();
+        else
+            eventQueue = new Queue<MusicEvent>(events);
+        nextEvent = eventQueue.Dequeue();
     }
 
     public void RegisterTap()
     {
         tapsDone += 1;
         AkSoundEngine.PostEvent(tapSound, gameObject);
-        this.GetComponentInChildren<ParticleSystem>().Emit(Mathf.CeilToInt(200/tapsToBreak));
+        this.GetComponentInChildren<ParticleSystem>().Emit(Mathf.CeilToInt(200 / tapsToBreak));
         Debug.Log("Tap");
+
+        latestEvent = CheckForEvents();
+        if (latestEvent != null)
+            TriggerEvent(latestEvent);
+    }
+
+    private MusicEvent CheckForEvents()
+    {
+        if (nextEvent.triggerEventAtPercentage >= tapsDone / tapsToBreak)
+            return nextEvent;
+        else
+            return null;
+
     }
 
     private void DestroyObstacle()
@@ -44,22 +69,30 @@ public class ObstacleHandler : MonoBehaviour
         if (obstacles.Count > 0)
         {
             Destroy(obstacles.Dequeue());
-            if (obstacles.Count < triggerEventAtPercentage)
-                TriggerEvent();
         }
         if (obstacles.Count <= 0)
+        {
             Destroy(this.gameObject);
+            AkSoundEngine.SetRTPCValue(nextEvent.audioClipName, 0.5f, ambienceSoundBank, 1000);
+        }
     }
 
-    private void TriggerEvent()
+
+
+    private void TriggerEvent(MusicEvent anEvent)
     {
-        Debug.Log("Event triggered!");
+        AkSoundEngine.SetRTPCValue(anEvent.audioClipName, anEvent.soundLevel, ambienceSoundBank, 1000);
+
+        //add sound call and event behavior
+        if (eventQueue.Count > 0)
+            nextEvent = eventQueue.Dequeue();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (obstacles.Count>0 && tapsDone*obstacles.Count >= tapsToBreak)
+        if (obstacles.Count > 0 && tapsDone * obstacles.Count >= tapsToBreak)
         {
             DestroyObstacle();
         }
---
+++ Assets/OurAssets/Gameplay/Core/Scripts/StoryScripts/MusicEvent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MusicEvent{
+
+    public string audioClipName;
+    public float soundLevel;
+    [Range(0, 1)]
+    public float triggerEventAtPercentage = 0.0f;
+
+}
